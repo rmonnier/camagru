8849421f706c69b9daa05410070eab78fdadde3c
Robin MONNIER <rmonnier@e1z3r5p6.42.us.org>
2017-03-14 17:27:16 -0700

    css 2.0

--- pages/profile.php
+++ pages/profile.php
@@ -9,9 +9,9 @@ require "templates/header_loggued.php";
 		<canvas id="canvas"></canvas>
 	</div>
 	<div class="container">
-		<img src="filters/apple.png" id="filter1" class="filter" alt="filter1">
+		<!-- <img src="filters/apple.png" id="filter1" class="filter" alt="filter1">
 		<img src="filters/palmier.png" id="filter2" class="filter" alt="filter2">
-		<img src="filters/minidoritos.png" id="filter3" class="filter selected" alt="filter3">
+		<img src="filters/minidoritos.png" id="filter3" class="filter selected" alt="filter3"> -->
 		Choose a filter :
 		<select name="filter-selector" id="filter-selector">
 			<option></option>
@@ -19,16 +19,17 @@ require "templates/header_loggued.php";
 			 <option value="filter2">Palmier</option>
 			 <option value="filter3">Doritos</option>
 		</select>
+		</div>
+		<div class="container">
+		<div id="upload">
+			<input type="file" id="fileUploaded">
+		</div>
 	</div>
 	<div class="container">
 	<button id="startbutton">Take a picture</button>
 	<button id="uploadbutton">Upload a picture</button>
 	</div>
 </section>
-<aside id="upload">
-    Select image to upload:
-  <input type="file" id="fileUploaded">
-</aside>
 <section class="gallery">
 	<?php foreach($db->getImg($_SESSION['loggued_on_user']) as $entry) { ?>
 			<?= $entry->displayImg(); ?>
